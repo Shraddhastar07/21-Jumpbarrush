a6c88f96011468b211ac68d7da0a67b288cbcbdf
laxminarasimhagude1995 <laxminarasimhagude@gmail.com>
2019-02-13 17:26:33 +0530

    laxmi1

---
+++ tewntyoneplus/ExhibitorViewController.h
@@ -0,0 +1,17 @@
+//
+//  ExhibitorViewController.h
+//  tewntyoneplus
+//
+//  Created by Kwebmaker on 13/02/2019.
+//  Copyright © 2019 kwebmaker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ExhibitorViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ tewntyoneplus/ExhibitorViewController.m
@@ -0,0 +1,32 @@
+//
+//  ExhibitorViewController.m
+//  tewntyoneplus
+//
+//  Created by Kwebmaker on 13/02/2019.
+//  Copyright © 2019 kwebmaker. All rights reserved.
+//
+
+#import "ExhibitorViewController.h"
+
+@interface ExhibitorViewController ()
+
+@end
+
+@implementation ExhibitorViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
